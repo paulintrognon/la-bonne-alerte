76756df9992231b5e59efd48dba95d818487ff58
Paulin Trognon <ptr@reportlinker.com>
2016-02-11 13:24:09 +0100

    add leboncoin service

---
+++ services/leboncoin.js
@@ -0,0 +1,67 @@
+"use strict";
+
+module.exports = {
+  create
+};
+
+const BPromise = require("bluebird"),
+  nightmareFactory = require("nightmare");
+
+function create() {
+  const that = {};
+
+  that.getItems = getItems;
+
+  return that;
+
+  function getItems(url) {
+    const nightmare = nightmareFactory();
+    return new BPromise(function (resolve, reject) {
+      /* globals scrollTo */
+      nightmare
+        .goto(url)
+        .evaluate(function () {
+          scrollTo(0, 2000);
+        })
+        .wait(500)
+        .evaluate(function () {
+          scrollTo(0, 3000);
+        })
+        .wait(500)
+        .evaluate(function () {
+          scrollTo(0, 4000);
+        })
+        .wait(500)
+        .evaluate(function () {
+          scrollTo(0, 5000);
+        })
+        .wait(500)
+        .evaluate(getItemsInPage)
+        .run(function (err, result) {
+          if (err) {
+            return reject(err);
+          }
+          resolve(result);
+        })
+        .end();
+    });
+  }
+}
+
+function getItemsInPage() {
+  /* globals $ */
+  var elements = [];
+  $(".list-lbc").children("a").each(function () {
+    var $element = $(this),
+      element = {
+        name: $element.prop("title"),
+        href: $element.prop("href"),
+        location: $(".placement", $element).first().text().replace(/[ \n\t]+/g, " ").trim(),
+        price: $(".price", $element).first().text().replace(/[ \n\t]+/g, " ").trim(),
+        imageUrl: $(".lbc .image .image-and-nb img", $element).prop("src")
+      };
+
+    elements.push(element);
+  });
+  return elements;
+}
