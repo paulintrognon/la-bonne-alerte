bff7db0c4062d76bc7de3dd41dbd525f158f6e81
Paulin Trognon <ptr@reportlinker.com>
2016-02-18 14:02:38 +0100

    add basic start script to test

--- start.js
+++ start.js
@@ -1,5 +1,11 @@
 "use strict";
 
-var logger = require("./services/logger.js");
+var leboncoin = require("./services/leboncoin.js").create();
 
-logger.info("Started!");
+leboncoin.getItems("http://www.leboncoin.fr/electromenager/offres/rhone_alpes/?f=a&th=1&ps=8&q=four&it=1&location=Lyon")
+  .then(function functionName(items) {
+    items.forEach(function (item) {
+      console.log(item);
+    });
+    console.log("end");
+  });
