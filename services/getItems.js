6c83f4be27413272c9d76a9d5b32fae0ad402bd1
Paulin Trognon <paulin.trognon@gmail.com>
2016-05-15 18:23:52 +0200

    add getItems based on crawlService

---
+++ services/getItems.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = getItems;
+
+const crawler = require('./crawler.js');
+
+function getItems(url) {
+  return crawler.crawlWithScroll(url, getItemsInPage);
+}
+
+function getItemsInPage() {
+  /* globals $ */
+  var elements = [];
+  $('.tabsContent').children('li').each(function () {
+    var $element = $('a', this).first();
+    var element = {
+      date: $('aside.item_absolute .item_supp', $element).first().text().replace(/[ \n\t]+/g, ' ').trim(),
+      name: $element.prop('title'),
+      href: $element.prop('href'),
+      location: $('.item_supp', $element).eq(1).text().replace(/[ \n\t]+/g, ' ').trim(),
+      price: $('.item_price', $element).first().text().replace(/[ \n\t]+/g, ' ').trim(),
+      imageUrl: $('.item_imagePic span img', $element).prop('src')
+    };
+
+    elements.push(element);
+  });
+  return elements;
+}
