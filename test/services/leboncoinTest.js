85b56033a29c7b056b695fe093cbf439e9192726
Paulin Trognon <paulin.trognon@gmail.com>
2016-05-16 23:09:29 +0200

    test leboncoin.completeItems

--- test/services/leboncoinTest.js
+++ test/services/leboncoinTest.js
@@ -2,10 +2,16 @@
 
 describe('leboncoin service', () => {
   describe('.getItems', getItemsSuite);
+  describe('.completeItems', completeItemsSuite);
 });
 
 function getItemsSuite() {
   it('should call getItems service then add ids', getItemsTest);
+}
+
+function completeItemsSuite() {
+  it('should return an empty array if items is empty', emptyItemsTest);
+  it('should call getItem per item given', completeItemsTest);
 }
 
 const BPromise = require('bluebird');
@@ -13,6 +19,8 @@ const path = '../../app/services/leboncoin.js';
 const proxyquire = require('proxyquire');
 const should = require('should');
 const sinon = require('sinon');
+
+// -----------------------------------------------------------
 
 function getItemsTest(done) {
   const items = [
@@ -42,8 +50,50 @@ function getItemsTest(done) {
     .then(done, done);
 }
 
+// -----------------------------------------------------------
+
+function emptyItemsTest(done) {
+  const getItemStub = sinon.stub();
+  const service = createService({ getItemStub });
+
+  service.completeItems([])
+    .then(res => {
+      should(getItemStub.callCount).equal(0);
+      should(res).eql([]);
+    })
+    .then(done, done);
+}
+
+function completeItemsTest(done) {
+  const getItemStub = sinon.stub();
+  const firstItem = { foo: 'first-item' };
+  const secondItem = { foo: 'second-item' };
+  const items = [
+    { id: 1 },
+    { id: 2 },
+  ];
+
+  getItemStub.onFirstCall().returns(BPromise.resolve(firstItem));
+  getItemStub.onSecondCall().returns(BPromise.resolve(secondItem));
+
+  const service = createService({ getItemStub });
+
+  service.completeItems(items)
+    .then(res => {
+      should(getItemStub.callCount).equal(2);
+      should(res).eql([
+        { foo: 'first-item', id: 1 },
+        { foo: 'second-item', id: 2 },
+      ]);
+    })
+    .then(done, done);
+}
+
+// -----------------------------------------------------------
+
 function createService(specs) {
   return proxyquire(path, {
-    './crawl/getItems.js': specs.getItemsStub,
+    './crawl/getItems.js': specs.getItemsStub || sinon.stub(),
+    './crawl/getItem.js': specs.getItemStub || sinon.stub(),
   });
 }
