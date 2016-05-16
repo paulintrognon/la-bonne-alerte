d1968e8e5229575528deb05f2a9e745e4e9efdf5
Paulin Trognon <paulin.trognon@gmail.com>
2016-05-17 00:36:55 +0200

    removing first image from carroussel

--- app/services/crawl/getItem.js
+++ app/services/crawl/getItem.js
@@ -27,11 +27,12 @@ function getItemInPage() {
     }
   });
 
-  item.images = [];
+  var images = [];
   $('.carousel .item_imagePic').each(function (element) {
     var src = $('img', this).first().attr('src');
-    item.images.push('http:' + src);
+    images.push('http:' + src);
   });
+  item.images = images.slice(1);
 
   item.description = $('p[itemprop="description"]').eq(0).html().trim();
   // item.phoneNumber = $('.sidebar span.phone_number').text().trim();
